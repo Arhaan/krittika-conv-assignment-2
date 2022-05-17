5003dd3fdfb6d8bff4806a651c203057501c1c5f
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-05-17 22:36:26 +0530

    Migrations for post

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,8 @@ from ckeditor.fields import RichTextField
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(default="Title", blank=False)
-    author = models.OneToOneField(User)
+    title = models.CharField(default="Title", blank=False, max_length=1000) # Not allowing more than 1000 characters here
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
     content = RichTextField()
     date_created = models.DateTimeField(auto_created=True)
 
